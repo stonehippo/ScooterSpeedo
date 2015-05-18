9290f7f5be69a88ad78ae57dda42bebfe7660f3c
George White <stonehippo@gmail.com>
2015-05-17 23:38:24 -0400

    Initial code (accelerometer Y axis only)

---
+++ ScooterSpeedo.ino
@@ -0,0 +1,59 @@
+/*
+  A wheel encoder speedometer and accelerometer for Zoe's scooter
+*/
+
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
+
+
+// 16x2 LCD display pins
+const byte LCD_BACKLIGHT = 5;
+const byte LCD_CONTRAST = 6;
+
+/*
+ * LCD RS pin to D13
+ * LCD Enable pin D12
+ * LCD D4 pin to D8
+ * LCD D5 pin to D9
+ * LCD D6 pin to D10
+ * LCD D7 pin to D11
+ */
+LiquidCrystal lcd(13, 12, 8, 9, 10, 11);
+
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+
+void setup() {
+  pinMode(LCD_CONTRAST, OUTPUT);
+  pinMode(LCD_BACKLIGHT, OUTPUT);
+  analogWrite(LCD_BACKLIGHT, 40);
+  analogWrite(LCD_CONTRAST, 120);
+  
+  lcd.begin(16,2);
+  
+  if (! mma.begin()) {
+    lcd.print("No accel start");
+    while (1);
+  }
+  mma.setRange(MMA8451_RANGE_8_G);
+  lcd.print("Ready!");
+}
+  
+void loop() {
+  mma.read();
+  
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  mma.getEvent(&event);
+
+  lcd.clear();
+  
+  lcd.print("Current Speed");
+  lcd.setCursor(0,1);
+  lcd.print("Y:");
+  lcd.print(event.acceleration.y);
+  lcd.print(" m/s^2");
+  delay(250);
+}
+
